b982a76b50ecfb00a9a41aac21c88e26cc5dac37
Sariot Hossain <sariothossain1011@gmail.com>
2024-05-03 23:07:46 +0600

    try to solve vercel error

--- src/ProductSkeleton/BrandSkeleton.jsx
+++ src/ProductSkeleton/BrandSkeleton.jsx
@@ -3,16 +3,16 @@ import Skeleton from "react-loading-skeleton";
 import Lottie from "lottie-react";
 import ImagePlaceholder from "../data/image.json"
 
-export function BrandSkeleton(props) {
+const BrandSkeleton =(props)=>{
     return ( 
         <div className="section">
         <div className="container">
             <div className="row">
                
                 {
-                    Array.from({length:8}).map((item,i)=>{
+                    Array.from({length:8}).map(()=>{
                         return(
-                            <div key={i+1} className="col-2 col-lg-8r text-center col-md-8r p-2">
+                            <div  className="col-2 col-lg-8r text-center col-md-8r p-2">
                                 <div className="card h-100 rounded-3 bg-white">
                                     <div className="card-body">
                                         <Lottie className="w-100" animationData={ImagePlaceholder} loop={true} />
@@ -30,3 +30,4 @@ export function BrandSkeleton(props) {
     );
 }
 
+export default BrandSkeleton
